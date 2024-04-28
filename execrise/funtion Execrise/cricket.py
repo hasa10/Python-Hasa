d61a4520108e9cd4d39947149994c1edc2d8f867
hasa10 <rasitha8000@gmail.com>
2024-04-28 10:20:59 +0530

    print over and ball

---
+++ execrise/funtion Execrise/cricket.py
@@ -0,0 +1,22 @@
+def start_game():
+    pass
+def overs_in_match():
+    over =0
+    while over <5 :
+        
+        ball_in_over(over) #positional argument
+        over+=1
+    else:
+        print(f'over: {over}.0')
+            
+def ball_in_over(over):
+    ball =0
+    while ball <6:
+        print(f'over: {over}.{ball}')
+        ball+=1
+    
+
+    
+def print_score():
+    pass
+overs_in_match()
